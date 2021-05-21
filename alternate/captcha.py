e281bae9ff548cf7301aef72b5d63ff154b27d1d
kushnagda <47942775+kushnagda@users.noreply.github.com>
2021-05-21 18:56:48 +0530

    Add files via upload

---
+++ alternate/captcha.py
@@ -0,0 +1,60 @@
+from svglib.svglib import svg2rlg
+from reportlab.graphics import renderPM
+import PySimpleGUI as sg
+import re
+from PIL import Image
+from anticaptchaofficial.imagecaptcha import imagecaptcha
+from twocaptchaapi import TwoCaptchaApi
+
+def captcha_builder(resp):
+    with open('captcha.svg', 'w') as f:
+        f.write(re.sub('(<path d=)(.*?)(fill="none"/>)', '', resp['captcha']))
+
+    drawing = svg2rlg('captcha.svg')
+    renderPM.drawToFile(drawing, "captcha.png", fmt="PNG")
+
+    im = Image.open('captcha.png')
+    im = im.convert('RGB').convert('P', palette=Image.ADAPTIVE)
+    im.save('captcha.gif')
+
+    layout = [[sg.Image('captcha.gif')],
+          [sg.Text("Enter Captcha Below")],
+          [sg.Input(key='input')],
+          [sg.Button('Submit', bind_return_key=True)]]
+
+    window = sg.Window('Enter Captcha', layout, finalize=True)
+    window.TKroot.focus_force()         # focus on window
+    window.Element('input').SetFocus()    # focus on field
+    event, values = window.read()
+    window.close()
+    return values['input']
+
+
+def captcha_builder_auto(resp, api_key, which_captcha):
+    with open('captcha.svg', 'w') as f:
+        f.write(re.sub('(<path d=)(.*?)(fill=\"none\"/>)', '', resp['captcha']))
+
+    drawing = svg2rlg('captcha.svg')
+    renderPM.drawToFile(drawing, "captcha.png", fmt="PNG")
+
+    
+    if which_captcha=='0': #anticaptchaofficial
+        solver = imagecaptcha()
+        solver.set_verbose(1)
+        solver.set_key(api_key)
+        captcha_text = solver.solve_and_return_solution("captcha.png")
+
+    elif which_captcha=='1': #twocaptchaapi
+        api = TwoCaptchaApi(api_key)
+        captcha_predicted = api.solve("captcha.png")
+        captcha_text = captcha_predicted.await_result()
+
+    else:
+        print("Invalid captcha API choice")
+
+    if captcha_text != 0:
+        print(f"Captcha text: {captcha_text}")
+    else:
+        print(f"Task finished with error: {solver.error_code}")
+
+    return captcha_text
